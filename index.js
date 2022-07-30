bbae7e0c3e01354ee9e92932d8eb800dc2a02727
Yahir Moreno <yahirmb7568@gmail.com>
2022-07-30 02:12:35 -0500

    Added postgreSQL

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
+const db = require('./queries.js')
 
 const app = express();
 const PORT = process.env.PORT || 3200;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Hola");
-})
+app.get('/', db.getTrainers)
+
+app.get('/trainers/:id', db.getTrainerById)
 
 app.listen(PORT, () => {
     console.log("Server running succesfully!")
