d63440594b5170695aa45cd19f93b7c4c213af47
Yahir Moreno <yahirmb7568@gmail.com>
2022-07-31 13:55:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = require('./routes.js');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3200;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/trainers', router)
 
